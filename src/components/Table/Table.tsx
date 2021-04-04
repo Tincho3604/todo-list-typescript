fe3e5029737cc9b7f215145aceafdc46193af26f
Tincho3604 <tincho3604@gmail.com>
2021-04-04 03:31:59 -0300

    feat: finished buttons

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,7 @@
 import React,{useState,useEffect} from 'react'
+import swal from 'sweetalert';
 import {Itask} from '../../interfaces/interfaces';
+import * as FcIcons from "react-icons/fc";
 import './style.css'
 
 interface Props {
@@ -9,8 +11,18 @@ interface Props {
 const Table = ({tasks,recover}:Props) => {
 
   const deleteTask = (id:number) => {
-    recover(tasks?.filter((item:Itask) =>  item?.id !== id))
+    swal("Your task has been deleted!", {
+      icon: "success",
+    });
+        recover(tasks?.filter((item:Itask) =>  item?.id !== id))
   }
+  
+
+  const changeStatus = (id:number) => {
+    recover(tasks?.filter((item:Itask) =>  (item.id === id && item.status === "Done") ? item.status =  "Pending" : item.status = 'Done'))
+    swal("Good job!", "You change the task status!", "success");
+  } 
+
 
 return(
 <>
@@ -22,19 +34,19 @@ return(
         <th scope="col">Date</th>
         <th scope="col">Status</th>
         <th scope="col">Delete</th>
-        <th scope="col">Edit</th>
+        <th scope="col">Change Status</th>
       </tr>
   </thead>
   <tbody>
   {tasks?.map((item:Itask,index:number) => {
       return( 
           <tr key={index}>
-              <td>{item.id}</td>
-              <td>{item.taskName}</td>
-              <td>{item.date}</td>
-              <td>{item.status}</td>
-              <td><button className="deleteButton" onClick={() => deleteTask(item.id)}>Delete</button></td>
-              <td><button className="editButton">Finished</button></td>
+              <td data-label="Id">{item.id}</td>
+              <td data-label="Name">{item.taskName}</td>
+              <td data-label="Date">{item.date}</td>
+              <td data-label="Status">{item.status === "Done" ? <FcIcons.FcOk/> : <FcIcons.FcHighPriority/>}</td>
+              <td data-label="Delete"><button className="deleteButton" onClick={() => deleteTask(item.id)}>Delete</button></td>
+              <td data-label="Change Status"><button className="editButton" onClick={() => changeStatus(item.id)}>Change Status</button></td>
           </tr>
         )
       })}
